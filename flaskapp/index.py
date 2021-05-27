7292e3b24a51df77abcea34b97a7b43fdacf804a
Elshadai <elshadaikassutegegn@gmail.com>
2021-05-27 14:40:02 +0300

    Bug fix

--- flaskapp/index.py
+++ flaskapp/index.py
@@ -10,10 +10,7 @@ from flaskapp.models import *
 class IndexAPI(Resource):
         
     def get(self, id=None):
-        message = ''' We're working on the documentation. Thank you for your patience.
-                    These are the routes that we got. 
-                    /institutions/, /institutions/<int:id>, /projects/, /projects/<int:id>, /users/, /users/<int:id>, /users/verify/<int:id>, /students/, /students/<int:id>, /teachers/, /teachers/<int:id>, /companies/, /companies/<int:id>, /works/, /works/<int:id>, /work/end/<int:id>, /rates/, /rates/<int:id>,
-                    '''
+        message = " We're working on the documentation. Thank you for your patience. These are the routes that we got. -------- /institutions/, /institutions/<int:id>, /projects/, /projects/<int:id>, /users/, /users/<int:id>, /users/verify/<int:id>, /students/, /students/<int:id>, /teachers/, /teachers/<int:id>, /companies/, /companies/<int:id>, /works/, /works/<int:id>, /work/end/<int:id>, /rates/, /rates/<int:id> "
         return message
 
     def delete(self, id=None):
